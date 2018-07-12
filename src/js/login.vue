51b5efd94174c0857c69faab36866aa1ed1f99c0
Chuanyu Wang <chuanyu.wang@sap.com>
2018-07-12 18:22:17 +0800

    Add dummy login control

--- src/js/login.vue
+++ src/js/login.vue
@@ -1,10 +1,17 @@
 <style>
-
 </style>
 
 <template lang="pug">
-v-container
-  div Login
+v-container(fill-height)
+  v-layout(justify-center align-center)
+    v-flex(md4 xs6)
+      v-form(ref="form" v-model="valid" lazy-validation)
+        v-text-field(v-model="email" :rules="emailRules" label="E-mail" required)
+        v-text-field(v-model='password' required :append-icon="show1 ? 'visibility_off' : 'visibility'"
+                    :type="show1 ? 'text' : 'password'" label="Password" @click:append="show1 = !show1")
+        v-checkbox(v-model="checkbox" label="Remeber me?")
+        v-btn(color="success" :disabled="!valid" @click="submit") Login
+        v-btn(@click="clear") Clear
 </template>
 
 <script>
@@ -21,13 +28,39 @@ module.exports = {
   },
   data: function() {
     return {
+      valid: true,
+      email: "",
+      emailRules: [
+        function(v) {
+          return !!v || "E-mail is required";
+        },
+        function(v) {
+          return /.+@.+/.test(v) || "E-mail must be valid";
+        }
+      ],
+      show1: false,
+      password: "",
+      checkbox: false
     };
   },
-  components: {
-  },
+  components: {},
   computed: {},
   filters: {},
-  methods: {},
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        // Native form submission is not yet supported
+        axios.post("/api/submit", {
+          email: this.email,
+          password: this.password,
+          checkbox: this.checkbox
+        });
+      }
+    },
+    clear() {
+      this.$refs.form.reset();
+    }
+  },
   created: function() {
     //var vm = this;
   },
