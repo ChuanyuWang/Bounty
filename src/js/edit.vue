a9374c1c29e2000a34ba0feb70a718823e0f8d5b
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2018-07-15 21:47:52 +0800

    add dummy page of edit

--- src/js/edit.vue
+++ src/js/edit.vue
@@ -3,12 +3,15 @@
 
 <template lang="pug">
 v-container
-  v-toolbar(flat color="white")
-    v-toolbar-title My Bouns Plan
-    v-divider.mx-2(inset vertical)
+  h2 2018年奖金计划表-生产部
+  v-subheader 刘总经理 {{ new Date().toLocaleString() }} {{$t('created')}}
+  v-toolbar(flat)
+    v-btn Import
+    v-btn Export
+    v-btn(color="info") Publish
     v-spacer
     v-dialog(v-model="dialog" max-width="500px")
-      v-btn.mb-2(slot="activator" color="primary" dark) New Item
+      v-btn.mb-2(slot="activator" color="primary" dark) Add Employee
       v-card
         v-card-title
           span.headline {{ formTitle }}
@@ -16,28 +19,31 @@ v-container
           v-container(grid-list-md)
             v-layout(wrap)
               v-flex(xs12 sm6 md4)
-                v-text-field(v-model="editedItem.name" label="Dessert name")
+                v-text-field(v-model="editedItem.name" :label="$t('employee_name')")
               v-flex(xs12 sm6 md4)
-                v-text-field(v-model="editedItem.calories" label="Calories")
+                v-text-field(v-model.number="editedItem.base" :label="$t('base')" mask='##############' prefix='$')
               v-flex(xs12 sm6 md4)
-                v-text-field(v-model="editedItem.fat" label="Fat (g)")
+                v-text-field(v-model.number="editedItem.bonus" :label="$t('bonus')" mask='##############' prefix='$')
               v-flex(xs12 sm6 md4)
-                v-text-field(v-model="editedItem.carbs" label="Carbs (g)")
-              v-flex(xs12 sm6 md4)
-                v-text-field(v-model="editedItem.protein" label="Protein (g)")
+                v-text-field(v-model.number="editedItem.performance" :label="$t('performance')" mask='###' suffix='%')
         v-card-actions
           v-spacer
-          v-btn(color="blue darken-1" flat @click.native="close") Cancel
-          v-btn(color="blue darken-1" flat @click.native="save") Save
-  v-data-table(:headers="headers" :items="desserts" hide-actions class="elevation-1")
+          v-btn(color="blue darken-1" flat @click.native="close") {{$t('cancel')}}
+          v-btn(color="blue darken-1" flat @click.native="save") {{$t('save')}}
+  v-data-table(:headers="headers" :items="employees" class="elevation-1")
     template(slot="items" slot-scope="props")
       td {{ props.item.name }}
-      td.text-xs-right {{ statusFormatter(props.item.status) }}
-      td.text-xs-right {{ props.item.amount }}
-      td.text-xs-right {{ props.item.quantity }}
+      td.text-xs-right {{ props.item.base | currencyFormatter }}
+      td.text-xs-right {{ props.item.bonus | currencyFormatter }}
+      td.text-xs-right {{ props.item.performance | percentFormatter }}
+      td.text-xs-right {{ props.item.total | currencyFormatter}}
       td.justify-center.layout.px-0
         v-icon.mr-2(small @click="editItem(props.item)") edit
         v-icon(small @click="deleteItem(props.item)") delete
+    template(slot="footer")
+      td(colspan='100%' style='text-align:right')
+        v-btn.ma-2(color="success") {{$t('calculate')}}
+    </template>
 </template>
 
 <script>
@@ -47,6 +53,8 @@ v-container
  * --------------------------------------------------------------------------
  */
 
+var data = require('./data');
+
 module.exports = {
   name: "editor",
   props: {
@@ -54,33 +62,33 @@ module.exports = {
   },
   data: function() {
     return {
+      valid: true,
       dialog: false,
       headers: [
         {
-          text: this.$t("name"),
+          text: this.$t("employee_name"),
           align: "left",
           value: "name"
         },
-        { text: this.$t("status"), value: "status" },
-        { text: this.$t("amount"), value: "amount" },
-        { text: this.$t("quantity"), value: "quantity" },
-        { text: this.$t("actions"), value: "status", sortable: false }
+        { text: this.$t("base"), value: "status", align: "right"},
+        { text: this.$t("bonus"), value: "bonus", align: "right" },
+        { text: this.$t("performance"), value: "performance", align: "right" },
+        { text: this.$t("total"), value: "total", align: "right" },
+        { text: this.$t("actions"), value: "name", sortable: false, align: "center" }
       ],
-      desserts: [],
+      employees: [],
       editedIndex: -1,
       editedItem: {
         name: "",
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
+        base: 0,
+        bonus: 0,
+        performance: 0
       },
       defaultItem: {
         name: "",
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
+        base: 0,
+        bonus: 0,
+        performance: 100
       }
     };
   },
@@ -92,67 +100,37 @@ module.exports = {
   },
   computed: {
     formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+      return this.editedIndex === -1 ? this.$t('new_employee') : this.$t('edit_employee');
     }
   },
-  filters: {},
+  filters: {
+    currencyFormatter(v) {
+      return '$' + (v||0).toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    },
+    percentFormatter(v) {
+      return v + '%'
+    }
+  },
   methods: {
     statusFormatter(value) {
       if (value === "released") return this.$t("released");
       if (value === "draft") return this.$t("draft");
       else return value;
     },
-    initialize() {
-      this.desserts = [
-        {
-          name: "2012年奖金计划表-销售部",
-          status: "released",
-          amount: 3000000,
-          quantity: 52
-        },
-        {
-          name: "2013年奖金计划表-生产部",
-          status: "released",
-          amount: 2000000,
-          quantity: 123
-        },
-        {
-          name: "2014年奖金计划表-销售部",
-          status: "released",
-          amount: 4000000,
-          quantity: 35
-        },
-        {
-          name: "2016年奖金计划表-生产部",
-          status: "released",
-          amount: 2000000,
-          quantity: 156
-        },
-        {
-          name: "2017年奖金计划表-销售部",
-          status: "released",
-          amount: 1000000,
-          quantity: 25
-        },
-        {
-          name: "2018年奖金计划表-生产部",
-          status: "draft",
-          amount: 2000000,
-          quantity: 135
-        }
-      ];
+    initialize(data) {
+      this.employees = data;
     },
 
     editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
+      this.editedIndex = this.employees.indexOf(item);
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
     },
 
     deleteItem(item) {
-      const index = this.desserts.indexOf(item);
+      const index = this.employees.indexOf(item);
       confirm("Are you sure you want to delete this item?") &&
-        this.desserts.splice(index, 1);
+        this.employees.splice(index, 1);
     },
 
     close() {
@@ -165,16 +143,16 @@ module.exports = {
 
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+        Object.assign(this.employees[this.editedIndex], this.editedItem);
       } else {
-        this.desserts.push(this.editedItem);
+        this.employees.push(this.editedItem);
       }
       this.close();
     }
   },
   created: function() {
     //var vm = this;
-    this.initialize();
+    this.initialize(data);
   },
   mounted: function() {
     //var vm = this;
