80034973334adca757f9954b979f4cfa35a9fdee
Chuanyu Wang <chuanyu.wang1983@gmail.com>
2018-07-18 22:10:56 +0800

    add animation for changing view
    support "slide-left", "slide-right" and "fade"

--- src/js/app.vue
+++ src/js/app.vue
@@ -1,5 +1,24 @@
-<style>
-
+<style lang="less">
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s ease;
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+.child-view {
+  position: absolute;
+  transition: all .5s cubic-bezier(.55,0,.1,1);
+}
+.slide-left-enter, .slide-right-leave-active {
+  opacity: 0;
+  -webkit-transform: translate(30px, 0);
+  transform: translate(30px, 0);
+}
+.slide-left-leave-active, .slide-right-enter {
+  opacity: 0;
+  -webkit-transform: translate(-30px, 0);
+  transform: translate(-30px, 0);
+}
 </style>
 
 <template lang="pug">
@@ -20,7 +39,8 @@ v-app
         v-list-tile-content
           v-list-tile-title {{$t('editor')}}
   v-content
-    router-view
+    transition(name='slide-right')
+      router-view.child-view
   v-footer(app)
     v-spacer
     label.caption 沪ICP备16016548号-3
